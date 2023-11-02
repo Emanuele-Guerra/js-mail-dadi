c29a8adf6b26528d68f6e5ad152ee8549c22ebf4
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-02 09:50:00 +0100

    push

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const utentiAutorizzati = ["emaguerra@gmail.com", "destroy-god@heaven.com", "javascript@script.com", "boolean@boolean.com"];
+
+
+const emailUtente = prompt("Inserisci la tua email:");
+
+let autorizzato = false;
+
+for (var i = 0; i < utentiAutorizzati.length; i++) {
+  if (utentiAutorizzati[i] === emailUtente) {
+    autorizzato = true;
+    break; 
+  }
+}
+
+
+if (autorizzato) {
+  alert("Accesso autorizzato. Benvenuto!");
+} else {
+  alert("Accesso negato. La tua email non è autorizzata.");
+}
